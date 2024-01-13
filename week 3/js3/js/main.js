ae0c1e2e99691a56a8fc078fe57cff34c5aedfda
Derek Smith Nautel <dsmithnautel@ufl.edu>
2024-01-13 14:57:15 -0500

    chapter 3 completed

---
+++ week 3/js3/js/main.js
@@ -0,0 +1,11 @@
+const myVariable = "Mathematics";
+
+console.log(myVariable.charAt(6));
+console.log(myVariable.lastIndexOf("at"));
+console.log(myVariable.slice(5));
+
+console.log(myVariable.toUpperCase());
+
+console.log(myVariable.includes("Math"));
+
+console.log(myVariable.split(""));
